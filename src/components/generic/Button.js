f8820d56c673faa9a164395d6cd02473343c8ebf
Tor Erik Alræk <torerik@vimond.com>
2018-04-10 22:52:28 +0200

    Added generic button component.

---
+++ src/components/generic/Button.js
@@ -0,0 +1,32 @@
+// @flow 
+import * as React from 'react';
+import { type CommonProps, prefixClassNames, defaultClassNamePrefix } from '../common';
+
+type Props = CommonProps & {
+    label?: string,
+    content?: React.Node,
+    onClick?: () => void
+};
+
+const baseClassName = 'button';
+
+class Button extends React.Component<Props> {
+    static defaultProps = {
+        classNamePrefix: defaultClassNamePrefix
+    }
+
+    handleClick = () => this.props.onClick && this.props.onClick();
+    
+    render() {
+        const {
+            label,
+            className,
+            classNamePrefix,
+            content
+        }: Props = this.props;
+        const classNames = prefixClassNames(classNamePrefix, baseClassName, className);
+        return <div title={label} onClick={this.handleClick} className={classNames}>{content}</div>;
+    }
+}
+
+export default Button;
---
+++ src/components/generic/Button.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Button from './Button';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test('Button renders with prefixed class name and DOM.', () => {
+    const rendered = shallow(<Button classNamePrefix="myplayer-" label="My button" className="myclassname" content="Click me!"/>);
+    expect(rendered.name()).toEqual('div');
+    expect(rendered.hasClass('myplayer-button')).toBe(true);
+    expect(rendered.hasClass('myplayer-myclassname')).toBe(true);
+    expect(rendered.props().title).toEqual('My button');
+    expect(rendered.text()).toEqual('Click me!');
+});
+
+test('Button invokes a callback when clicked', () => {
+    const handleClick = jest.fn();
+    const rendered = shallow(<Button onClick={handleClick} classNamePrefix="myplayer-" className="myclassname" content="Click me!"/>);
+    rendered.simulate('click');
+    expect(handleClick).toHaveBeenCalled();
+});
