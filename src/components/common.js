4b7bdb353af01cb55c625e5f2c43a0b41af4028b
Tor Erik Alræk <torerik@vimond.com>
2018-04-23 23:08:03 +0200

    Added time formatting helpers.

--- src/components/common.js
+++ src/components/common.js
@@ -73,6 +73,38 @@ export function override(base: ?{}, overrides: ?{}): {} {
     }
 	return copy;
 }
+
+export const formatTimeComponent = (integer: number, separator: string = '', emptyIfZero: boolean = false, pad: boolean = true) => {
+	return (emptyIfZero && integer === 0) ? '' : (integer < 10 && pad ? `0${integer}${separator}` : `${integer}${separator}`);
+};
+
+export const formatTime = (seconds: number) => {
+	let rounded = Math.round(seconds);
+	let negativeMark = '';
+	if (seconds === true || isNaN(seconds) || seconds === Infinity) {
+		rounded = 0;
+	} else if (rounded < 0) {
+		rounded = -rounded;
+		negativeMark = '-';
+	}
+	const days = Math.floor(rounded / 86400);
+	const daysInSeconds = days * 86400;
+	const hours   = Math.floor((rounded - daysInSeconds) / 3600);
+	const hoursAndDaysInSeconds = daysInSeconds + (hours * 3600);
+	const minutes = Math.floor((rounded - hoursAndDaysInSeconds) / 60);
+	const secs = rounded - hoursAndDaysInSeconds - (minutes * 60);
+	return negativeMark + formatTimeComponent(days, '.', true, false) + formatTimeComponent(hours, ':', days === 0) + formatTimeComponent(minutes, ':', false) + formatTimeComponent(secs);
+};
+
+export const formatClockTime = (date: Date) => {
+	const isValidDate = date instanceof Date && !isNaN(date.getTime());
+	const hours = isValidDate ? date.getHours() : 0;
+	const minutes = isValidDate ? date.getMinutes() : 0;
+	const seconds = isValidDate ? date.getSeconds() : 0;
+	
+	return formatTimeComponent(hours, ':', false) + formatTimeComponent(minutes, ':', false) + formatTimeComponent(seconds);
+};
+
 /*
 export const withProps = (Comp: React.ComponentType<{}>, injectedProps: any) => {
 	return (props: any) => <Comp {...injectedProps} {...props} />; // Warning: props wins. Is that desiresd?
--- src/components/common.test.js
+++ src/components/common.test.js
@@ -1,4 +1,4 @@
-import { prefixClassNames, getBoundingEventCoordinates } from "./common"
+import { prefixClassNames, getBoundingEventCoordinates, formatTime, formatClockTime } from "./common"
 
 test('prefixClassNames() prefixes all class names passed and joins into one string.', () => {
     const prefix = 'myprefix-';
@@ -87,6 +87,50 @@ test('getBoundingEventCoordinates() doesn\'t return coordinates outside element 
     expect(getBoundingEventCoordinates(mockEvent1)).toMatchObject({ x: 143, y: 40, width: 400, height: 40 });
     expect(getBoundingEventCoordinates(mockEvent2)).toMatchObject({ x: 0, y: 0, width: 400, height: 40 });
     expect(getBoundingEventCoordinates(mockEvent3)).toMatchObject({ x: 400, y: 40, width: 400, height: 40 });
+});
+
+test('formatTime() formats seconds with decimals into days, hours, (if > 0), and always minutes and seconds.', () =>  {
+	expect(formatTime(34.56)).toBe('00:35');
+	expect(formatTime(123.45)).toBe('02:03');
+	expect(formatTime(4567.89)).toBe('01:16:08');
+	expect(formatTime(-4567.89)).toBe('-01:16:08');
+	expect(formatTime(87654.321)).toBe('1.00:20:54');
+});
+
+test('formatTime() formats 0 values and invalid numbers as 00:00.', () =>  {
+	const zz = '00:00';
+	expect(formatTime(NaN)).toBe(zz);
+	expect(formatTime(undefined)).toBe(zz);
+	expect(formatTime(null)).toBe(zz);
+	expect(formatTime('Midnight')).toBe(zz);
+	expect(formatTime('')).toBe(zz);
+	expect(formatTime(false)).toBe(zz);
+	expect(formatTime(true)).toBe(zz);
+	expect(formatTime(0)).toBe(zz);
+});
+
+const pad = v => (v < 10 ? '0' : '') + v;
+
+const getUtcTime = (year, month, day, timeStr) => {
+	const numbers = timeStr.split(':');
+	const date = new Date(year, month + 1, day, parseInt(numbers[0],10), parseInt(numbers[1],10), parseInt(numbers[2],10));
+	return pad(date.getUTCHours()) + ':' + pad(date.getUTCMinutes()) + ':' + pad(date.getUTCSeconds());
+};
+
+test('formatClockTime() formats dates with hours, minutes, and seconds in local time.', () => {
+	// Time zones spoil these tests.
+	expect(getUtcTime(2018, 4, 23, formatClockTime(new Date('2018-04-23T20:56:12.123Z')))).toEqual('20:56:12');
+	expect(getUtcTime(2018, 4, 22, formatClockTime(new Date('2018-04-22T23:59:59.567Z')))).toEqual('23:59:59');
+});
+
+test('formatClockTime() formats invalid dates as 00:00:00.', () => {
+	const zzz = '00:00:00';
+	expect(formatClockTime(new Date(NaN))).toBe(zzz);
+	expect(getUtcTime(1970, 1, 1, formatClockTime(new Date(0)))).toBe(zzz);
+	expect(formatClockTime(new Date(Infinity))).toBe(zzz);
+	expect(formatClockTime(null)).toBe(zzz);
+	expect(formatClockTime('Midnight')).toBe(zzz);
+	expect(formatClockTime(123)).toBe(zzz);
 });
 
 test('override() merges two objects deeply, also when branches are unspecified in one of them.');
