db69a8c7b5be7599721fd7738d9175addc157963
Tor Erik Alræk <torerik@vimond.com>
2018-04-23 19:23:57 +0200

    Completed play/pause button test. Added default class name.

--- src/components/controls/PlayPauseButton.js
+++ src/components/controls/PlayPauseButton.js
@@ -1,33 +1,39 @@
 // @flow 
 import * as React from 'react';
 import ToggleButton from '../generic/ToggleButton';
+import { defaultClassNamePrefix } from '../common';
 
 type Props = {
     isPaused?: boolean,
     updateProperty?: ({ isPaused: boolean }) => void,
     label?: string,
     playingContent: React.Node,
-    pausedContent: React.Node
+    pausedContent: React.Node,
+    classNamePrefix: string
 };
 
 const className = 'play-pause-button';
 
 class PlayPauseButton extends React.Component<Props> {
+    static defaultProps = {
+        classNamePrefix: defaultClassNamePrefix
+    };
     
     handleToggle = (value: boolean) => {
         if (this.props.updateProperty) {
 			this.props.updateProperty({ isPaused: value });
         }
-    }
+    };
 
     render() {
         const {
             isPaused,
             pausedContent,
             playingContent,
-            label
+            label,
+            classNamePrefix
         } = this.props;
-        return <ToggleButton isOn={isPaused} className={className} label={label} onToggle={this.handleToggle} toggledOnContent={pausedContent} toggledOffContent={playingContent}/>
+        return <ToggleButton classNamePrefix={classNamePrefix} isOn={isPaused} className={className} label={label} onToggle={this.handleToggle} toggledOnContent={pausedContent} toggledOffContent={playingContent}/>
     }
     //TODO: Move definition of default class name prefix from generic components into this level.
 }
