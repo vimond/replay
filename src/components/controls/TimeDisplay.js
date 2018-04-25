4450a14a833a5ec9c2350a06a9a8f3c73400f6aa
Tor Erik Alræk <torerik@vimond.com>
2018-04-25 22:51:02 +0200

    Completed some tests with functionality for TimeDisplay.

--- src/components/controls/TimeDisplay.js
+++ src/components/controls/TimeDisplay.js
@@ -1,4 +1,54 @@
-import * as React from 'react';
+// @flow
 
-const TimeDisplay = () => <div id="time"/>;
+import * as React from 'react';
+import type { CommonProps } from '../common';
+import { formatClockTime, formatTime, prefixClassNames } from '../common';
+import type { PlayMode } from '../player/VideoStream/common';
+
+type Props = CommonProps & {
+	position: number,
+	duration?: number,
+	absolutePosition?: ?Date,
+	playMode?: PlayMode,
+	liveDisplayMode: 'clock-time' | 'live-offset',
+	positionLabel?: string,
+	durationLabel?: string,
+	clockTimeLabel?: string
+};
+
+const className = 'time-display';
+const positionClassName = 'time-display-position';
+const durationClassName = 'time-display-duration';
+const clockTimeClassName = 'time-display-clock-time';
+const noDurationClassName = 'time-display-no-duration';
+
+const isReasonableDateTime = date => date instanceof Date && !isNaN(date.getTime()) && date.getTime() > 1514761200000;
+
+const formatAndLimitTime = (time?: number, zeroAndBelow: boolean = false) => formatTime(time == null ? 0 : Math[zeroAndBelow ? 'min' : 'max'](0, time));
+
+const TimeDisplay = ({ position, duration, absolutePosition, playMode, liveDisplayMode, label, positionLabel, durationLabel, clockTimeLabel, classNamePrefix }: Props) => {
+	if (playMode === 'ondemand') {
+		return (
+			<div className={prefixClassNames(classNamePrefix, className)} title={label}>
+				<span className={prefixClassNames(classNamePrefix, positionClassName)} title={positionLabel}>{formatAndLimitTime(position)}</span>
+				<span className={prefixClassNames(classNamePrefix, durationClassName)} title={durationLabel}>{formatAndLimitTime(duration)}</span>
+			</div>
+		);
+	} else {
+		if ((liveDisplayMode === 'live-offset' && playMode === 'livedvr') || !isReasonableDateTime(absolutePosition)) {
+			return (
+				<div className={prefixClassNames(classNamePrefix, className)} title={label}>
+					<span className={prefixClassNames(classNamePrefix, positionClassName)} title={positionLabel}>{formatAndLimitTime(position, true)}</span>
+					<span className={prefixClassNames(classNamePrefix, durationClassName)} title={durationLabel}>{formatAndLimitTime(duration)}</span>
+				</div>
+			);
+		} else {
+			return (
+				<div className={prefixClassNames(classNamePrefix, className, noDurationClassName)} title={label}>
+					<span className={prefixClassNames(classNamePrefix, clockTimeClassName)} title={positionLabel}>{formatClockTime(absolutePosition)}</span>
+				</div>
+			);
+		}
+	}
+};
 export default TimeDisplay;
--- src/components/controls/TimeDisplay.test.js
+++ src/components/controls/TimeDisplay.test.js
@@ -25,33 +25,36 @@ const dates = {
 	invalid4: undefined
 };
 
-const renderTimeDisplay = ({ playMode, position, duration, absolutePosition, liveDisplayMode="clockTime", classNamePrefix="a-", positionLabel="Position", durationLabel="Duration", clockTimeLabel="Time" }) => {
+const renderTimeDisplay = ({ playMode, position, duration, absolutePosition, liveDisplayMode="clockTime", classNamePrefix="a-", label="Playback time", positionLabel="Position", durationLabel="Duration", clockTimeLabel="Time" }) => {
 	return shallow(
 		<TimeDisplay 
 			classNamePrefix={classNamePrefix}
 			liveDisplayMode={liveDisplayMode}
+			playMode={playMode}
 			absolutePosition={absolutePosition}
 			position={position}
 			duration={duration}
+			label={label}
 			positionLabel={positionLabel}
 			durationLabel={durationLabel}
 			clockTimeLabel={clockTimeLabel}
-
 		/>);
 };
 
-test.skip('TimeDisplay renders with prefixed class name and DOM including children for position and duration.', () => {
-	const rendered = renderTimeDisplay({ position: 0, duration: numbers.normal, playMode: 'ondemand' });
-	expect(rendered.props().label).toBe('C');
+test('TimeDisplay renders with prefixed class name and DOM including children for position and duration.', () => {
+	const rendered = renderTimeDisplay({ position: 0, duration: 123, playMode: 'ondemand' });
+	expect(rendered.prop('title')).toBe('Playback time');
 	expect(rendered.hasClass('a-time-display')).toBe(true);
 	const positionElement = rendered.find('.a-time-display-position');
 	const durationElement = rendered.find('.a-time-display-duration');
 	const clockTimeElement = rendered.find('.a-time-display-clock-time');
-	expect(clockTimeElement).toBe(null);
+	expect(clockTimeElement.length).toBe(0);
 	expect(positionElement.name()).toBe('span');
 	expect(durationElement.name()).toBe('span');
-	expect(positionElement.text()).toBe('0:00');
-	expect(durationElement.text()).toBe('0:20');
+	expect(positionElement.text()).toBe('00:00');
+	expect(durationElement.text()).toBe('02:03');
+	expect(positionElement.prop('title')).toBe('Position');
+	expect(durationElement.prop('title')).toBe('Duration');
 });
 
 test('TimeDisplay in on demand mode formats negative positions or durations as 00:00.', () => {
@@ -61,6 +64,8 @@ test('TimeDisplay in on demand mode formats negative positions or durations as 0
 test('TimeDisplay with clock time mode displays no duration, but a time of day including hours, minutes, and seconds, for live streams with valid time stamps.', () => {});
 
 test('TimeDisplay with live offset mode or without valid time stamps displays duration and offset/timeshift from live position, including hours, minutes, and seconds.', () => {});
+
+test('TimeDisplay with live offset mode, and play mode live, attempts displaying position as clock time.', () => {});
 
 test('TimeDisplay renders 00:00 on falsy positions, and no duration element on falsy duration.', () => {});
 
