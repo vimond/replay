0a71e84427f4a0723c5bc8b367eb761fca24d26c
Tor Erik Alræk <torerik@vimond.com>
2018-06-24 19:51:51 +0200

    Added JSDoc example.

--- src/replay/components/generic/ToggleButton.js
+++ src/replay/components/generic/ToggleButton.js
@@ -3,10 +3,15 @@ import * as React from 'react';
 import { type CommonGenericProps, prefixClassNames } from '../common';
 
 type Props = CommonGenericProps & {
+  /** Set to true if the button should be in the toggled on mode. */
   isOn?: boolean,
+  /** The label will appear in the title attribute of the root DOM element of the toggle button. */
   label?: string,
+  /** The button content to be displayed when the button is toggled off. */
   toggledOffContent?: React.Node,
+  /** The button content to be displayed when the button is toggled on. */
   toggledOnContent?: React.Node,
+  /** A callback method that will be invoked when the button is clicked and the value toggled. If the button has been toggled on, true is passed to the callback. */
   onToggle?: boolean => void
 };
 
@@ -14,6 +19,9 @@ const baseClassName = 'toggle-button';
 const offClassName = 'toggled-off';
 const onClassName = 'toggled-on';
 
+/**
+ * Renders a button with two states - toggled on and off. When clicked, it reports back the opposite state.
+ */
 class ToggleButton extends React.Component<Props> {
   handleClick = () => this.props.onToggle && this.props.onToggle(!this.props.isOn);
 
