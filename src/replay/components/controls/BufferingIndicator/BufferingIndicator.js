a650ef43f55095ed30be2703f1c7929baf23d4f7
Tor Erik Alræk <torerik@vimond.com>
2018-11-08 15:36:08 +0100

    Docs WIP.

--- src/replay/components/controls/BufferingIndicator/BufferingIndicator.js
+++ src/replay/components/controls/BufferingIndicator/BufferingIndicator.js
@@ -6,11 +6,17 @@ import type { CommonProps } from '../../common';
 import type { ObservingControlStaticProps } from '../../player/PlayerController/ControllerContext';
 
 type Props = CommonProps & {
+  /** ⇘︎ If true, the buffering indicator is shown or the root element has a class name indicating buffering. */
   isBuffering?: boolean,
+  /** ⇘︎ If true, the buffering indicator is shown or the root element has a class name indicating buffering. */
   isSeeking?: boolean,
+  /** ⇘︎ If set to 'starting', 'buffering', or 'seeking', the buffering indicator is shown or the root element has a class name indicating buffering. */
   playState?: PlayState,
+  /** The content to show if buffering, or always if renderingStrategy is 'always'. */
   children?: React.Node,
+  /** Alternative to specifying children. Other Replay controls use content as the convention. */
   content?: React.Node,
+  /** When set to 'always', the content is always rendered, but buffering is indicated through a class name on the component's root element. This option is used when controlling display through CSS. */
   renderStrategy?: 'when-buffering' | 'always'
 };
 
@@ -28,7 +34,7 @@ const BufferingIndicator: BufferingIndicatorType = ({
   renderStrategy = 'when-buffering',
   label,
   classNamePrefix = defaultClassNamePrefix
-}) => {
+}: Props) => {
   const isActive =
     isBuffering || isSeeking || playState === 'starting' || playState === 'buffering' || playState === 'seeking';
   if (renderStrategy === 'always') {
