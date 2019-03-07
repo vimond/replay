f9638d3e8e19e14dc7e578c49c5b5988aa6a93cd
Tor Erik Alræk <torerik@vimond.com>
2019-03-07 04:46:58 +0100

    Added React strict mode.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
 registerServiceWorker();
